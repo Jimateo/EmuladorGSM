f3f2472d5ce562cc14b8dffe65009262b4164ebf
Jimateo <nachomateo912@uma.es>
2024-05-28 17:53:27 +0200

    Actulizacion Codigo

--- GSM.py
+++ GSM.py
@@ -2,42 +2,44 @@ from Crypto.Cipher import AES
 from Crypto.Random import get_random_bytes
 import os
 
+# Implementación ficticia de MILENAGE para la demostración
+def milenage(Ki, OPc, RAND):
+    # Esta es una implementación simplificada. Debes usar la implementación real del algoritmo MILENAGE.
+    cipher = AES.new(Ki, AES.MODE_ECB)
+    SRES = cipher.encrypt(RAND)[:4]  # Respuesta esperada
+    Kc = cipher.encrypt(RAND)[:16]  # Clave de cifrado temporal
+    IK = cipher.encrypt(RAND)[:16]  # Clave de integridad temporal
+    AUTN = cipher.encrypt(RAND)[:16]  # Número de autenticación
+    return SRES, Kc, IK, AUTN
+
 class Movil:
-    def __init__(self, IMSI, Ki):
+    def __init__(self, IMSI, Ki, OPc):
         self.IMSI = IMSI
         self.Ki = Ki
+        self.OPc = OPc
         self.RAND = None
         self.SRES_ = None
         self.Kc = None
+        self.IK = None
     
     def set_RAND(self, RAND):
         self.RAND = RAND
 
-    # Implementación del Algoritmo A3
-    def a3_algorithm(self, Ki, RAND):
-        cipher = AES.new(Ki, AES.MODE_ECB)
-        SRES_ = cipher.encrypt(RAND)[:4]  # SRES será de 32 bits (4 bytes)
-        self.SRES_ = SRES_
-    
-    # Implementación del Algoritmo A8
-    def a8_algorithm(self, Ki, RAND):
-        cipher = AES.new(Ki, AES.MODE_ECB)
-        Kc = cipher.encrypt(RAND)[:8]  # Kc será de 64 bits (8 bytes)
-        self.Kc = Kc
+    def authenticate(self):
+        self.SRES_, self.Kc, self.IK, _ = milenage(self.Ki, self.OPc, self.RAND)
 
-    # Implementación de la función ENC
-    def ENC(self):
-        cipher = AES.new(self.Kc + self.Kc, AES.MODE_ECB)
-        msg = cipher.encrypt(b'Hola000000000000')
-        return msg
+    def ENC(self, message):
+        cipher = AES.new(self.Kc, AES.MODE_ECB)
+        mensaje_padding = message.ljust(16, b'\0')
+        msg_enc = cipher.encrypt(mensaje_padding)
+        return msg_enc
 
 class Antena:
-
     def __init__(self):
         self.IMSI = None
         self.RAND = None
         self.SRES = None
-        self.Kc = None     
+        self.Kc = None
         self.SRES_ = None
         self.msg = None
 
@@ -58,55 +60,46 @@ class Antena:
     def read_msg(self, msg):
         self.msg = msg
 
-    # Implementación de la función ENC
     def DEC(self):
-        cipher = AES.new(self.Kc + self.Kc, AES.MODE_ECB)
+        cipher = AES.new(self.Kc, AES.MODE_ECB)
         msg_dec = cipher.decrypt(self.msg)
         return msg_dec
 
-class Operador: 
-
+class Operador:
     def __init__(self):
         self.RAND = None
         self.Ki = None
+        self.OPc = None
 
     def get_KI(self, IMSI):
         if IMSI == "214050000000095":
             self.Ki = b'1234567890123456'
-    
+            self.OPc = b'6543210987654321'
+
     def generate_RAND(self):
         self.RAND = get_random_bytes(16)
 
-    # Implementación del Algoritmo A3
-    def a3_algorithm(self):
-        cipher = AES.new(self.Ki, AES.MODE_ECB)
-        SRES = cipher.encrypt(self.RAND)[:4]  # SRES será de 32 bits (4 bytes)
-        return SRES
-    
-    # Implementación del Algoritmo A8
-    def a8_algorithm(self):
-        cipher = AES.new(self.Ki, AES.MODE_ECB)
-        Kc = cipher.encrypt(self.RAND)[:8]  # Kc será de 64 bits (8 bytes)
-        return Kc    
+    def authenticate(self):
+        SRES, Kc, IK, AUTN = milenage(self.Ki, self.OPc, self.RAND)
+        return SRES, Kc
 
     def get_parameters(self, IMSI):
-        SRES = self.a3_algorithm()
-        Kc = self.a8_algorithm() 
+        SRES, Kc = self.authenticate()
         return IMSI, self.RAND, SRES, Kc
-
 # ------------------------ MAIN ------------------------
 
 IMSI = "214050000000095"
 Ki = b'1234567890123456'
+OPc =  b'6543210987654321'
 
-movil = Movil(IMSI,Ki)
+movil = Movil(IMSI, Ki, OPc)
 antena = Antena()
 operador = Operador()
 
 # Enviamos IMSI del móvil a la antena
 antena.get_IMSI(movil.IMSI)
 
-# Enviamos el IMSI de la antena al Operador para así obtener el Ki y generamos los parámetros
+# Enviamos el IMSI de la antena al Operador para así obtener el Ki y OPc y generar los parámetros
 operador.get_KI(antena.IMSI)
 operador.generate_RAND()
 IMSI, RAND, SRES, Kc = operador.get_parameters(antena.IMSI)
@@ -118,18 +111,15 @@ antena.set_parameters(RAND, SRES, Kc)
 movil.set_RAND(antena.RAND)
 
 # El móvil genera SRES' y se la envía a la antena
-movil.a3_algorithm(movil.Ki, movil.RAND)
+movil.authenticate()
 antena.set_SRES_(movil.SRES_)
 
 # Antena comprueba que es correcto y envía un OK
 if antena.validate_authentication():
-    
-    print("El SRES generado por el móvil coincide por el obtenido de la operadora.")
-    movil.a8_algorithm(movil.Ki, movil.RAND)
-    antena.read_msg(movil.ENC())
+    print("El SRES generado por el móvil coincide con el obtenido de la operadora.")
+    msg_encrypted = movil.ENC(b'Hola000000000000')
+    antena.read_msg(msg_encrypted)
     print(f'Mensaje cifrado: {antena.msg}')
     print(f'Mensaje descifrado: {antena.DEC()}')
-
 else:
-    print("El SRES generado por el móvil no coincide por el obtenido de la operadora.")
-
+    print("El SRES generado por el móvil no coincide con el obtenido de la operadora.")
